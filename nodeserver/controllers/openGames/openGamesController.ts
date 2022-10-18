225836875082915885e1c628c1744b8e99245f3d
SadPlum <sebastienolegault@gmail.com>
2022-10-18 19:56:01 -0300

    Set up base models and controller

--- nodeserver/controllers/openGames/openGamesController.ts
+++ nodeserver/controllers/openGames/openGamesController.ts
@@ -1,5 +1,45 @@
-import { allOpenGames } from "../../models/openGames/openGameModel";
+import { allOpenGames, openGame } from "../../models/openGames/openGameModel";
 
-export const getAllGames = () => {
-  return allOpenGames();
+export const getAllGames = async (req, res) => {
+  try {
+    // Gets all open games
+    const games = await allOpenGames();
+    // If games cannot be found, throw error
+    if (!games) {
+      throw new Error("Gannot find games");
+    }
+    res.status(200).json({
+      status: "success",
+      data: games,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
+
+export const getGame = async (req, res) => {
+  try {
+    // Search for games in openGames collection based on difficulty
+    // Difficulty is taken as param
+    const gameDifficulty = req.params.difficulty;
+    const game = await openGame(gameDifficulty);
+    // if game is returned undefined, throw error
+    if (!game) {
+      throw new Error("Cannot find game");
+    }
+    res.status(200).json({
+      status: "success",
+      data: game,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: err.message,
+    });
+  }
 };
--- nodeserver/models/openGames/openGameModel.ts
+++ nodeserver/models/openGames/openGameModel.ts
@@ -1,5 +1,26 @@
 import { OpenGames } from "./openGameSchema";
+import { difficultyArray } from "../../utils/gameData";
 
-export const allOpenGames = () => {
-  return OpenGames.find({});
+export const allOpenGames = async () => {
+  // Find all open games, array of 6 games
+  const search = await OpenGames.find({}).maxTimeMS(10000);
+  // Sort by game id (0-5, from easiest to hardest)
+  const sorted = search.sort((a, b) => {
+    return a.id - b.id;
+  });
+  // return
+  return sorted;
+};
+
+export const openGame = async (diff) => {
+  // Ensure param is valid difficulty
+  if (difficultyArray.find((diff) => diff) === undefined) {
+    return undefined;
+  }
+  // Find one open game by difficulty string taken from req.param
+  const search = await OpenGames.findOne({ difficulty: `${diff}` }).maxTimeMS(
+    10000
+  );
+  // return
+  return search;
 };
