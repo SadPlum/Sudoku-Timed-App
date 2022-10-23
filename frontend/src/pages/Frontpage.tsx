cbd719a473c808e67e7fa89bfe0a498482c65b06
SadPlum <92168542+SadPlum@users.noreply.github.com>
2022-10-23 18:10:46 -0300

    push back to pc

--- frontend/src/pages/Frontpage.tsx
+++ frontend/src/pages/Frontpage.tsx
@@ -1,48 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DisplayGame from "../components/displayGame/DisplayGame";
+import { getAllOpenGames } from "../functions/api/apiCalls";
 
 function Frontpage() {
+  const [games, setGames] = React.useState<any[] | undefined>(undefined);
   const tempScores = [
     { name: "abc", time: 12.11 },
     { name: "def", time: 15.12 },
     { name: "last", time: 18.2 },
   ];
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/opengames")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        console.log(data.data);
+        setGames(data.data);
+      });
+  }, []);
+
   return (
     <main className="frontpage">
-      {" "}
       <div className="displayGameBox-container">
         <div className="displayGameBox">
-          <DisplayGame
-            difficulty="beginner"
-            difficultyNums={40}
-            scores={tempScores}
-          />
-          <DisplayGame
-            difficulty="easy"
-            difficultyNums={35}
-            scores={tempScores}
-          />
-          <DisplayGame
-            difficulty="medium"
-            difficultyNums={30}
-            scores={tempScores}
-          />
-          <DisplayGame
-            difficulty="hard"
-            difficultyNums={25}
-            scores={tempScores}
-          />
-          <DisplayGame
-            difficulty="evil"
-            difficultyNums={20}
-            scores={tempScores}
-          />
-          <DisplayGame
-            difficulty="diabolical"
-            difficultyNums={17}
-            scores={tempScores}
-          />
-        </div>{" "}
+          {games &&
+            games.map((game) => (
+              <DisplayGame
+                key={game.id}
+                difficulty={game.difficulty}
+                difficultyNums={40}
+                scores={tempScores}
+              />
+            ))}
+        </div>
       </div>
     </main>
   );
